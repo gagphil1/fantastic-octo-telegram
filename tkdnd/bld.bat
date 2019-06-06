871e64923526fa8708776fc4a018418147cb0f2a
Philippe Gagne <philippe.gagne@optelgroup.com>
2019-06-06 12:31:56 -0400

    Compile windows working.

--- tkdnd/bld.bat
+++ tkdnd/bld.bat
@@ -1,6 +1,4 @@
-  :: A different SDK is needed when build with VS 2017 and 2015
-  :: http://wiki.tcl.tk/54819
-    call "%VS140COMNTOOLS%..\..\VC\vcvarsall.bat" x64 10.0.15063.0
+call "%VS140COMNTOOLS%..\..\VC\vcvarsall.bat" x64 10.0.15063.0
 
 cd win
 nmake "TCL=%BUILD_PREFIX%\library" TCL_VERSION=8.6 TCL_VERSION_NO_DOTS=86 MACHINE=AMD64
