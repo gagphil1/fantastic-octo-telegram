9fe92aff9832d4cf50a782dd98ad6509636dd777
Philippe Gagné <philippemgagne@icloud.com>
2019-06-10 22:04:04 -0400

    Cleanup.

--- tkinterdnd2/setup.py
+++ tkinterdnd2/setup.py
@@ -8,10 +8,6 @@ shutil.copy("demo_files_and_text.py","demo")
 shutil.copy("demo_megawidgets.py", "demo")
 shutil.copy("demo_simple_text.py", "demo")
 
-f = open('demo/__init__.py', 'a')
-print("from demo import *", file=f)
-f.close()
-
 shutil.move('demo', 'TkinterDnD2')
 
 setup(
